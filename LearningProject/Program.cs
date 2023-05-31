2d5b684c39b13d25ec26c48131a0e62f0d751125
FarzanaNasrin633 <fnasrin633@gmail.com>
2023-05-31 20:25:16 +1000

    Initial commit

---
+++ LearningProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace LearningProject
+{   
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Farzana Nasrin";
+            int age = 29;
+           // var fullName2 = "Rezaul Hasan";
+            Console.WriteLine(fullName);
+            Console.WriteLine(age);
+
+
+        }
+
+    }
+}
